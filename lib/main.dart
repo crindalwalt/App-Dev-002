6294ea9cfe13a461d9bb0c7da4058053a88e35ee
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-09-10 13:22:37 +0500

    container

--- lib/main.dart
+++ lib/main.dart
@@ -8,28 +8,34 @@ void main() {
 }
 
 class HomePage extends StatelessWidget {
+  String content = "Shahzad Farooq";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.pinkAccent,
-        title: Text(
+        title: const Text(
           "Nutri addso",
           style: TextStyle(color: Colors.white),
         ),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Text(
-              "I have created my first app",
-              style: TextStyle(fontSize: 30.00),
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 97, 150, 0),
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+          width: 200,
+          // height: 100,
+          child: Text(
+            content,
+            textAlign: TextAlign.center,
+            style: const TextStyle(
+              color: Colors.white,
             ),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Click here"),
-            ),
-          ],
+          ),
         ),
       ),
     );
