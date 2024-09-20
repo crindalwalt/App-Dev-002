3433164f037f2e16698b4ca8a7f7a54752b6b9fe
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-09-20 12:57:12 +0500

    loops done

---
+++ language/004_loops_revised.dart
@@ -0,0 +1,126 @@
+void main() {
+  int age = 3;
+  List<String> friendsList = ["Shahzad", "Muhthsim", "Zaid"];
+  List<int> marksList = [23, 45, 56, 74, 2, 4, 6774, 646, 35, 353];
+  print(friendsList[2]);
+  print(marksList[6]);
+  print(friendsList.length);
+  print(marksList.length);
+  // loop are used to execute a block of code over and over again by a controlled condition
+  // 3 types of loops
+  // for loop
+  // while loop
+  // do while loop
+  // 3 constraints of loop
+  // initilazaiton (ak variable bnana jo loop ko control kre ga)
+  // condition (jb tak condition true rhy hi loop chaly gi warna ni)
+  // updating (loop variable ko update krna)
+
+  List<String> pakistaniNames = [
+    "Abdul Ahad",
+    "Abdul Azeem",
+    "Abdul Basit",
+    "Abdul Hadi",
+    "Abdul Jabbar",
+    "Abdul Kareem",
+    "Abdul Mannan",
+    "Abdul Qadir",
+    "Abdul Rauf",
+    "Abdul Samad",
+    "Abdur Rahman",
+    "Ahmad Khan",
+    "Akbar Ali",
+    "Ali Asghar",
+    "Ali Raza",
+    "Amir Hussain",
+    "Arif Mahmood",
+    "Asadullah",
+    "Atif Ali",
+    "Awais Khan",
+    "Bilal Ahmed",
+    "Babar Iqbal",
+    "Danish Ali",
+    "Ehsan Ullah",
+    "Fahad Ali",
+    "Farhan Qureshi",
+    "Fazal Karim",
+    "Ghulam Abbas",
+    "Ghulam Farid",
+    "Hafiz Saeed",
+    "Hamza Khan",
+    "Hassan Naqvi",
+    "Hashim Raza",
+    "Hidayat Ullah",
+    "Hussain Shah",
+    "Iftikhar Ahmed",
+    "Irfan Ali",
+    "Imran Khan",
+    "Iqbal Hussain",
+    "Jameel Khan",
+    "Javed Iqbal",
+    "Kashif Ali",
+    "Khalid Mehmood",
+    "Khurram Shahzad",
+    "Majid Ali",
+    "Mansoor Ahmed",
+    "Mohammad Ashraf",
+    "Mohammad Iqbal",
+    "Mohammad Saleem",
+    "Mohammad Usman",
+    "Muneer Ahmed",
+    "Nadeem Akhtar",
+    "Naeem Ullah",
+    "Nasir Abbas",
+    "Nawaz Sharif",
+    "Noman Khan",
+    "Osama Ali",
+    "Qasim Shah",
+    "Rashid Minhas",
+    "Rauf Siddiqui",
+    "Rehan Ali",
+    "Sajid Iqbal",
+    "Salman Ahmed",
+    "Samiullah Khan",
+    "Shahid Ali",
+    "Shafqat Hussain",
+    "Shakeel Ahmed",
+    "Sharjeel Khan",
+    "Shoaib Akhtar",
+    "Sikandar Ali",
+    "Sohail Khan",
+    "Syed Ali",
+    "Syed Hussain",
+    "Tahir Abbas",
+    "Talha Ali",
+    "Tanveer Ahmed",
+    "Ubaid Ullah",
+    "Usman Ghani",
+    "Waqar Ali",
+    "Waseem Ahmed",
+    "Yasir Hussain",
+    "Zafar Abbas",
+    "Zahid Khan",
+    "Zain Ali",
+    "Zeeshan Ahmed",
+    "Adnan Ahmed",
+    "Adeel Khan",
+    "Aamir Raza",
+    "Asif Mehmood",
+    "Bashir Ahmed",
+    "Faisal Khan",
+    "Ghulam Murtaza",
+    "Hamid Ali",
+    "Imtiaz Ahmed",
+    "Junaid Khan",
+    "Kashan Ali",
+    "Moazzam Khan",
+    "Naveed Iqbal",
+    "Raheel Khan",
+    "Saad Ahmed",
+    "Zulfiqar Ali"
+  ];
+
+  for (int index = 0; index < pakistaniNames.length; index++) {
+    print(pakistaniNames[index]);
+  }
+}
