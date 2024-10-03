419796800f0929b39e6a0631b58264883011a3f1
Shahzad Farooq <shahzadfarooqdev@gmail.com>
2024-10-03 12:56:17 +0500

    rows and coloumn widget done

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,40 @@ class EntryPoint extends StatelessWidget {
 
     // );
     // return page;
+    String greeting = "Hola Amigo";
+    Widget puranaColumn = Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(greeting),
+        const Icon(Icons.home),
+        const Image(
+          height: 300,
+          // image: NetworkImage("https://picsum.photos/200/200"),
+          image: AssetImage("assets/images/box_bg.jpg"),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          child: Icon(Icons.search),
+        ),
+      ],
+    );
+    Color primaryColor = const Color.fromARGB(255, 211, 179, 39);
+    Widget containerx = Container(
+      margin: EdgeInsets.all(15),
+      padding: EdgeInsets.all(20),
+      decoration: BoxDecoration(
+          color: Colors.amber, borderRadius: BorderRadius.circular(20)),
+      width: double.infinity,
+      child: Text(
+        "Card 1",
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+    );
 
-    Color primaryColor = Colors.green;
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 163, 164, 165),
       appBar: AppBar(
@@ -32,8 +64,24 @@ class EntryPoint extends StatelessWidget {
           )
         ],
       ),
-      body: const Center(
-        child: Text("Hy There"),
+      body: Container(
+        width: double.infinity,
+        color: const Color.fromARGB(255, 161, 228, 165),
+        child: SingleChildScrollView(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(
+                Icons.home,
+                color: Colors.blue,
+              ),
+              Text("Click Now"),
+              // Image.network("https://picsum.photos/200/200")
+              IconButton(onPressed: () {}, icon: Icon(Icons.search))
+            ],
+          ),
+        ),
       ),
       floatingActionButton: Container(
         width: 130,
